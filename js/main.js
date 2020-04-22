d83e0a493ffca7f32f9b4daa71eebe05d278663a
Clinton Enyinna <enyinnaclinton@gmail.com>
2020-04-22 18:31:01 -0500

    bug: fix linter errors

--- js/main.js
+++ js/main.js
@@ -1,101 +1,79 @@
 let myLibrary = [];
-let table = document.querySelector("table");
-let getTableErr = document.querySelector("#tb-err");
-let getBookButton = document.querySelector("#add-book");
-let getBookForm = document.querySelector("#book-form");
-getBookForm.style.display = "None";
-let getUserErr = document.querySelector("#userinputerr");
-let getDeleteBtn = document.querySelector("#book-delete");
-let getEditBtn = document.querySelector("#book-edit");
+const table = document.querySelector('table');
+const getTableErr = document.querySelector('#tb-err');
+const getBookButton = document.querySelector('#add-book');
+const getBookForm = document.querySelector('#book-form');
+getBookForm.style.display = 'None';
+const getUserErr = document.querySelector('#userinputerr');
+const getDeleteBtn = document.querySelector('#book-delete');
+const getEditBtn = document.querySelector('#book-edit');
 
-const myLibraryData = JSON.parse(localStorage.getItem("myLibrary"));
+const myLibraryData = JSON.parse(localStorage.getItem('myLibrary'));
 if (myLibraryData) {
   myLibrary = myLibraryData;
 }
 
-getBookForm.addEventListener("submit", function (event) {
-  event.preventDefault();
-
-  if (
-    getBookForm.elements[0].value != "" &&
-    getBookForm.elements[1].value != "" &&
-    getBookForm.elements[0].value != ""
-  ) {
-    getUserErr.innerHTML = "";
-
-    addBookToLibrary(
-      getBookForm.elements[0].value,
-      getBookForm.elements[1].value,
-      getBookForm.elements[2].value
-    );
-    getUserErr.innerHTML = "Book added";
-    setTimeout(function () {
-      window.location.reload();
-    }, 1000);
-  } else {
-    getUserErr.innerHTML = "All Fields must not be empty";
-  }
-  getBookForm.elements[0].value = "";
-  getBookForm.elements[1].value = "";
-  getBookForm.elements[2].value = "";
-});
-
-getBookButton.addEventListener("click", function (event) {
-  event.preventDefault();
-  getBookForm.style.display = "block";
-});
-
-function Book(title, author, pages, read = "No") {
+function Book(title, author, pages, read = 'No') {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
 }
 
-
 function addBookToLibrary(title, author, pages) {
-  let newBook = new Book(title, author, pages);
+  const newBook = new Book(title, author, pages);
   myLibrary.push(newBook);
-  localStorage.setItem("myLibrary", JSON.stringify(myLibrary));
+  localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
 }
 
-function render(table, myLibrary) {
-  generateTableHead(table, myLibrary);
-  generateTableContent(table, myLibrary);
-}
+getBookForm.addEventListener('submit', (event) => {
+  event.preventDefault();
 
-function generateTableHead(table, myLibrary) {
-  if (myLibrary.length < 1) {
-    getTableErr.innerHTML = "Database empty";
+  if (
+    getBookForm.elements[0].value !== ''
+    && getBookForm.elements[1].value !== ''
+    && getBookForm.elements[0].value !== ''
+  ) {
+    getUserErr.innerHTML = '';
+
+    addBookToLibrary(
+      getBookForm.elements[0].value,
+      getBookForm.elements[1].value,
+      getBookForm.elements[2].value,
+    );
+    getUserErr.innerHTML = 'Book added';
+    setTimeout(() => {
+      window.location.reload();
+    }, 1000);
   } else {
-    let thead = table.createTHead();
-    let row = thead.insertRow();
-    let data = Object.keys(myLibrary[0]);
-    let th = document.createElement("th");
-    let text = document.createTextNode("Operations");
-    th.appendChild(text);
-    row.appendChild(th);
-    for (let key of data) {
-      let th = document.createElement("th");
-      let text = document.createTextNode(key.toUpperCase());
+    getUserErr.innerHTML = 'All Fields must not be empty';
+  }
+  getBookForm.elements[0].value = '';
+  getBookForm.elements[1].value = '';
+  getBookForm.elements[2].value = '';
+});
+
+getBookButton.addEventListener('click', () => {
+  getBookForm.style.display = 'block';
+});
+
+function generateTableHead() {
+  if (myLibrary.length < 1) {
+    getTableErr.innerHTML = 'Database empty';
+  } else {
+    const thead = table.createTHead();
+    const row = thead.insertRow();
+    const data = Object.keys(myLibrary[0]);
+    const thh = document.createElement('th');
+    const headText = document.createTextNode('Operations');
+    thh.appendChild(headText);
+    row.appendChild(thh);
+    data.forEach((key) => {
+      const th = document.createElement('th');
+      const text = document.createTextNode(key.toUpperCase());
       th.appendChild(text);
       row.appendChild(th);
-    }
-  }
-}
-
-function generateTableContent(table, myLibrary) {
-  let tbody = table.createTBody();
-
-  for (let element of myLibrary) {
-    let row = tbody.insertRow();
-    row.innerHTML =
-      '<input type="checkbox" class="myinput" id="' + getRowId() + '">';
-    for (let key in element) {
-      let cell = row.insertCell();
-      let text = document.createTextNode(element[key]);
-      cell.appendChild(text);
-    }
+    });
   }
 }
 
@@ -105,37 +83,55 @@ function getRowId() {
   return rowIndex;
 }
 
-getDeleteBtn.addEventListener("click", function () {
-  let getMyInput = document.querySelectorAll(".myinput");
-  for (let index = 0; index < getMyInput.length; index++) {
+function generateTableContent() {
+  const tbody = table.createTBody();
+
+  myLibrary.forEach((element) => {
+    const row = tbody.insertRow();
+    row.innerHTML = `<input type="checkbox" class="myinput" id="${getRowId()}">`;
+    Object.keys(element).forEach((key) => {
+      if (key) {
+        const cell = row.insertCell();
+        const text = document.createTextNode(element[key]);
+        cell.appendChild(text);
+      }
+    });
+  });
+}
+
+function render() {
+  generateTableHead(table, myLibrary);
+  generateTableContent(table, myLibrary);
+}
+
+getDeleteBtn.addEventListener('click', () => {
+  const getMyInput = document.querySelectorAll('.myinput');
+  for (let index = 0; index < getMyInput.length; index += 1) {
     if (getMyInput[index].checked) {
-      let rowId = getMyInput[index].id;
+      const rowId = getMyInput[index].id;
       delete myLibrary[rowId - 1];
     }
   }
-  let temp = myLibrary.filter((i) => i);
+  const temp = myLibrary.filter((i) => i);
 
-  localStorage.setItem("myLibrary", JSON.stringify(temp));
+  localStorage.setItem('myLibrary', JSON.stringify(temp));
   window.location.reload();
 });
 
-getEditBtn.addEventListener("click", function () {
-  let getMyInput = document.querySelectorAll(".myinput");
-  for (let index = 0; index < getMyInput.length; index++) {
+getEditBtn.addEventListener('click', () => {
+  const getMyInput = document.querySelectorAll('.myinput');
+  for (let index = 0; index < getMyInput.length; index += 1) {
     if (getMyInput[index].checked) {
-      let rowId = getMyInput[index].id;
-      if (myLibrary[rowId - 1].read == "Yes") {
-        myLibrary[rowId - 1].read = "No" 
-      }else{
-        myLibrary[rowId - 1].read = "Yes"
-      } 
-
-      
-      
+      const rowId = getMyInput[index].id;
+      if (myLibrary[rowId - 1].read === 'Yes') {
+        myLibrary[rowId - 1].read = 'No';
+      } else {
+        myLibrary[rowId - 1].read = 'Yes';
+      }
     }
   }
 
-  localStorage.setItem("myLibrary", JSON.stringify(myLibrary));
+  localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
   window.location.reload();
 });
 
